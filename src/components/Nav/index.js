be9b80c530dbcd5623aa205abbf5c3393a839e1f
Sam Morgen <samorgen14@gmail.com>
2021-09-11 13:19:06 -0700

    initial navigation bar

---
+++ src/components/Nav/index.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+function Nav() {
+  return (
+    <header>
+      <h2>
+        <a href="/">Sam Morgen</a>
+      </h2>
+      <nav>
+        <ul>
+          <li>About me</li>
+          <li>Portfolio</li>
+          <li>Contact</li>
+          <li>Resume</li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
+
+export default Nav;
