ca316be706492ad066489535f14e5dc1a26ffb46
Sam Morgen <samorgen14@gmail.com>
2021-09-12 14:42:23 -0700

    initial footer attempt

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import About from "./components/About";
 import Portfolio from "./components/Portfolio";
 import Contact from "./components/Contact";
 import Resume from "./components/Resume";
+import Footer from "./components/Footer";
 
 function App() {
   const [sections] = useState([
---
+++ src/components/Footer/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <footer>
+      <h2>Contact</h2>
+      <ul>
+        <li>
+          <a href="mailto:samorgen14@gmail.com">samorgen14@gmail.com</a>
+        </li>
+      </ul>
+    </footer>
+  );
+}
+
+export default Footer;
