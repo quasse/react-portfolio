9a4a7e733becfe55d1650e733f8311b49d9e8716
Sam Morgen <samorgen14@gmail.com>
2021-09-12 14:19:12 -0700

    resume page styling

--- src/components/Resume/index.js
+++ src/components/Resume/index.js
@@ -1,13 +1,17 @@
 import React from "react";
+import resume from "../../assets/files/Resume.pdf";
 
 function Resume() {
   return (
     <section className="resume">
       <h2>Resume</h2>
       <p>
-        Download my resume <a href="../../assets/files/Resume.pdf">here</a>
+        Download my resume{" "}
+        <a href={resume} download>
+          here
+        </a>
       </p>
-      <div className="front-end-proficiencies">
+      <div className="proficiencies">
         <h3>Front end proficiences</h3>
         <ul>
           <li>HTML</li>
@@ -16,12 +20,12 @@ function Resume() {
           <li>React</li>
         </ul>
       </div>
-      <div className="back-end-proficiencies">
+      <div className="proficiencies">
         <h3>Back end proficiences</h3>
         <ul>
           <li>MONGODB</li>
           <li>SQL</li>
-          <li>Other stuff</li>
+          <li>Express.js</li>
         </ul>
       </div>
     </section>
