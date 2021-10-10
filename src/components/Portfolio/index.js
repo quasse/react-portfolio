9926e37447ca30e6101fd4a9adce46db7a1aa2b2
Sam Morgen <samorgen14@gmail.com>
2021-10-10 16:43:47 -0700

    delete some portfolio items

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -3,22 +3,6 @@ import React from "react";
 function Portfolio() {
   const projects = [
     {
-      name: "Game Library",
-      img: require("../../assets/images/game-screenshot.png").default,
-      link: "https://attsgames.herokuapp.com/",
-    },
-    {
-      name: "Crypto Tracker",
-      img: require("../../assets/images/crypto-tracker-screenshot.png").default,
-      link: "https://warm-coast-72937.herokuapp.com/",
-    },
-    {
-      name: "Stock News",
-      img: require("../../assets/images/stock-news-screenshot-crop.png")
-        .default,
-      link: "https://quasse.github.io/group-project/",
-    },
-    {
       name: "Note Taker",
       img: require("../../assets/images/note-taker-screenshot.png").default,
       link: "https://shielded-waters-60571.herokuapp.com/",
